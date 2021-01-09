bcce80ef747f686d2890bea4aa50202fc7830053
anureet19 <anureet021999@gmail.com>
2021-01-09 15:13:10 +0530

    Text validation

--- app/src/main/java/com/anureet/tictactoe/PlayerDetailFragment.kt
+++ app/src/main/java/com/anureet/tictactoe/PlayerDetailFragment.kt
@@ -1,10 +1,13 @@
 package com.anureet.tictactoe
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_player_detail.*
 
@@ -26,14 +29,50 @@ class PlayerDetailFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val playerOne = player1Name.text.toString()
-        val playerTwo = player2Name.text.toString()
-
-        beginGameButton.setOnClickListener {
-            findNavController().navigate(
-                PlayerDetailFragmentDirections.actionPlayerDetailFragmentToMultiplayerFragment(playerOne,playerTwo)
-            )
-        }
+        beginGameButton.isEnabled = false
+        validateText()
     }
+
+    private fun validateText(){
+        val nameTextWatcher = object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int){
+                val playerOne = player1Name.text.toString()
+                val playerTwo = player2Name.text.toString()
+
+                when {
+                    playerOne.isEmpty() -> {
+                        beginGameButton.isEnabled = false
+                        player1Name.error = "Player Name cannot be empty!"
+            //                    Toast.makeText(activity,"Player Name cannot be empty!",Toast.LENGTH_SHORT).show()
+                    }
+                    playerTwo.isEmpty() -> {
+                        beginGameButton.isEnabled = false
+                        player2Name.error = "Player Name cannot be empty!"
+                    }
+                    else -> {
+                        val playerOne = player1Name.text.toString()
+                        val playerTwo = player2Name.text.toString()
+                        beginGameButton.isEnabled = true
+            //                    val names: List<String> = listOf(playerOne,playerTwo)
+                        beginGameButton.setOnClickListener {
+                            findNavController().navigate(
+                                PlayerDetailFragmentDirections.actionPlayerDetailFragmentToMultiplayerFragment(
+                                    playerOne,playerTwo
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        player1Name.addTextChangedListener(nameTextWatcher)
+        player2Name.addTextChangedListener(nameTextWatcher)
+
+    }
+
 
 }
