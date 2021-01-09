7381b94480733a471a77e022a9db1e01e26a3504
anureet19 <anureet021999@gmail.com>
2021-01-10 02:54:54 +0530

    Added Animations

--- app/src/main/java/com/anureet/tictactoe/ui/MultiplayerFragment.kt
+++ app/src/main/java/com/anureet/tictactoe/ui/MultiplayerFragment.kt
@@ -1,26 +1,34 @@
 package com.anureet.tictactoe.ui
 
 import android.graphics.Typeface
+import android.graphics.drawable.AnimationDrawable
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.anureet.tictactoe.R
 import com.anureet.tictactoe.data.Victory
 import kotlinx.android.synthetic.main.fragment_multiplayer.*
 
+
 class MultiplayerFragment : Fragment() {
     private var playerOneTurn = true
+
+    // To store moves of players
     private var playerOneMoves = mutableListOf<Int>()
     private var playerTwoMoves = mutableListOf<Int>()
 
-
+    // Setting up possible wins
     private val possibleWins: Array<List<Int>> = arrayOf(
         //horizontal lines
         listOf(1, 2, 3),
@@ -36,6 +44,9 @@ class MultiplayerFragment : Fragment() {
         listOf(1, 5, 9),
         listOf(3, 5, 7)
     )
+
+    // For animation
+    private lateinit var balloonAnimation: AnimationDrawable
 
     private lateinit var viewModel: VictoryDetailViewModel
 
@@ -57,6 +68,8 @@ class MultiplayerFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        // Calling function to check if the player's record is available or not
+        // Updating the record
         IncrementIfMatchFound()
 
         val playerOne = MultiplayerFragmentArgs.fromBundle(
@@ -66,6 +79,7 @@ class MultiplayerFragment : Fragment() {
             requireArguments()
         ).Player2Name
 
+        // Setting up player names on textview
         player_one_label.text = playerOne
         player_two_label.text = playerTwo
 
@@ -73,6 +87,7 @@ class MultiplayerFragment : Fragment() {
         setupBoard()
     }
 
+    // If a player is replaying, this function increments the game count
     private fun IncrementIfMatchFound() {
         // Incrementing game count
         var flag = 0
@@ -83,6 +98,8 @@ class MultiplayerFragment : Fragment() {
         val playerTwo = MultiplayerFragmentArgs.fromBundle(
             requireArguments()
         ).Player2Name
+
+        // List of all the objects present in the database
         viewModel.victory.observe(viewLifecycleOwner, Observer {
             if (it.isNotEmpty() && flag == 0) {
                 flag = 1
@@ -113,6 +130,7 @@ class MultiplayerFragment : Fragment() {
         })
     }
 
+    // Setting up board
     private fun setupBoard(){
         var counter = 1
 
@@ -130,16 +148,10 @@ class MultiplayerFragment : Fragment() {
             linearLayout.orientation = LinearLayout.HORIZONTAL
             linearLayout.layoutParams = params1
             params1.weight  = 1.0F
-//            params1.setMargins(5,5,5,5)
 
             for(j in 1..3){
                 val button = ImageButton(requireActivity())
                 button.id = counter
-//                button.textSize = 64.0F
-//                button.setTextColor(ContextCompat.getColor(requireActivity(),
-//                    R.color.outside
-//                ))
-
                 button.setBackgroundResource(R.drawable.shadow_button)
 
                 button.layoutParams = params2
@@ -154,40 +166,53 @@ class MultiplayerFragment : Fragment() {
         }
     }
 
+    // Recording each move in array as button ids
     private fun recordMove(view: View){
         val button = view as ImageButton
         val id = button.id
 
+        val playerOne = MultiplayerFragmentArgs.fromBundle(
+            requireArguments()
+        ).Player1Name
+        val playerTwo = MultiplayerFragmentArgs.fromBundle(
+            requireArguments()
+        ).Player2Name
+
         if(playerOneTurn){
             playerOneMoves.add(id)
 
-//            button.text = "O"
             button.setImageResource(R.drawable.o)
             button.isEnabled = false
             if(checkWin(playerOneMoves)){
                 saveDetail(1)
-//                showWinMessage(player_one)
+                showWinMessage(playerOne)
             } else{
                 playerOneTurn = false
+
+                // It will toggle the turn
                 togglePlayerTurn(player_two_label, player_one_label)
             }
 
         } else{
             playerTwoMoves.add(id)
 
-//            button.text = "X"
             button.setImageResource(R.drawable.x)
             button.isEnabled = false
+
             if(checkWin(playerTwoMoves)){
                 saveDetail(2)
-//                showWinMessage(player_two)
+                showWinMessage(playerTwo)
             } else{
                 playerOneTurn = true
+
+                // It will toggle the turn
                 togglePlayerTurn(player_one_label, player_two_label)
             }
         }
     }
 
+    // Saving detail if a new player is playing
+    // Updating detail if the player's record exist
     private fun saveDetail(playerNum: Int) {
         var match = false
         var flag = 0
@@ -251,7 +276,7 @@ class MultiplayerFragment : Fragment() {
 
     }
 
-
+    // checking for a win by comparing it with possible wins array
     private fun checkWin(moves: List<Int>): Boolean{
         var won = false
         if(moves.size >= 3){
@@ -267,6 +292,7 @@ class MultiplayerFragment : Fragment() {
         return won
     }
 
+    // Toggle the turn and highlight player's name
     private fun togglePlayerTurn(playerOn: TextView, playerOff: TextView){
         playerOff.setTextColor(
             ContextCompat.getColor(requireActivity(),
@@ -280,11 +306,56 @@ class MultiplayerFragment : Fragment() {
         playerOn.setTypeface(null, Typeface.BOLD)
     }
 
-    private fun showWinMessage(player: EditText){
-        var playerName = player.text.toString()
-        if(playerName.isBlank()){
-            playerName = player.hint.toString()
+    // Shows a dialog box when a player wins
+    private fun showWinMessage(player: String){
+        // To add animation
+        animation()
+
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setTitle("Congratulations!")
+        builder.setMessage("$player WON")
+        builder.setIcon(R.drawable.cool)
+
+        //It will restart the existing game
+        builder.setPositiveButton("Restart Game"){dialogInterface, which ->
+            refreshFragment()
         }
+
+        //It will navigate to the main menu
+        builder.setNegativeButton("Main Page"){dialogInterface, which ->
+            findNavController().navigate(
+                MultiplayerFragmentDirections.actionMultiplayerFragmentToMainPageFragment()
+            )
+        }
+        // Create the AlertDialog
+        val alertDialog: AlertDialog = builder.create()
+
+        // Set other dialog properties
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+
+    }
+
+    // Balloon and confetti will pop up on a win
+    private fun animation() {
+        // Added Animation
+        val balloonImage = requireActivity().findViewById<ImageView>(R.id.balloonView)
+        balloonImage.setBackgroundResource(R.drawable.animation)
+        balloonAnimation = balloonImage.background as AnimationDrawable
+
+        balloonAnimation.start()
+    }
+
+    // Refreshes Fragment
+    fun refreshFragment() {
+        playerOneMoves = mutableListOf()
+        playerTwoMoves = mutableListOf()
+        val ft: FragmentTransaction = requireFragmentManager().beginTransaction()
+        if (Build.VERSION.SDK_INT >= 26) {
+            ft.setReorderingAllowed(false)
+        }
+        ft.detach(this).attach(this).commit()
+
 
     }
 
